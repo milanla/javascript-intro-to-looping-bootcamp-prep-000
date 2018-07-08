026225fccf70378af56c395ef384dcb3f3f764b6
Milan Sacher <milan.lanlada@gmail.com>
2018-07-08 04:13:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,8 +21,9 @@ function whileLoop(n) {
 function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5
-  } do {
-    array.pop();
+  } 
+  
+  do { array.pop();
   } while array.length > 0 && maybeTrue());
     return array;
 }
